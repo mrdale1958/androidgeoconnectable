1165c9cc5fa2d10db9f9d564bd94e35e8e45c62e
mrdale1958 <dale.macdonald@utdallas.edu>
2017-11-22 23:33:39 -0600

    idle works, lat lon indicators done

--- app/src/main/java/com/botherconsulting/geoconnectable/OuterCircleTextView.java
+++ app/src/main/java/com/botherconsulting/geoconnectable/OuterCircleTextView.java
@@ -4,73 +4,72 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.graphics.Path;
-import android.view.View;
+import android.util.AttributeSet;
 
 /**
  * Created by dalemacdonald on 11/21/17.
  */
 
-public class OuterCircleTextView extends View {
+public class OuterCircleTextView extends android.support.v7.widget.AppCompatTextView {
 
-        private String MY_TEXT = "xjaphx: Draw Text on Curve";
-        private Path mArc;
+    private String MY_TEXT = "xjaphx: Draw Text on Curve";
+    private Path mArc;
+    private float hOffset;
+    private float vOffset;
 
-        private Paint mPaintText;
-
-        public OuterCircleTextView(Context context) {
-            super(context);
-            setPath(20,20, 200, 200, -180, 200);
-            setTextStyle(Paint.ANTI_ALIAS_FLAG, Paint.Style.FILL_AND_STROKE, Color.WHITE,20f);
-
-        }
-
-        public OuterCircleTextView(Context context, String newText) {
-            super(context);
-            setText(newText);
-        }
-
-        public void setText(String newText) {
-            MY_TEXT = newText;
-            invalidate();
-        }
-
-        public void setTextStyle (int flags, Paint.Style style, int color, float size) {
-            mPaintText = new Paint(flags);
-            mPaintText.setStyle(style);
-            mPaintText.setColor(color);
-            mPaintText.setTextSize(size);
-            invalidate();
-        }
-
-        public void setPath(int left,
-                            int right,
-                            int top,
-                            int bottom,
-                            int startAngle,
-                            int sweepAngle) {
-            mArc = new Path();
-            RectF oval = new RectF(left,top,right,bottom);
-            mArc.addArc(oval, startAngle, sweepAngle);
-
-        }
-
-        public void setPath(RectF newOval,
-                            int startAngle,
-                            int sweepAngle) {
-            mArc = new Path();
-            RectF oval = newOval;
-            mArc.addArc(oval, startAngle, sweepAngle);
-
-        }
-
-
-
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            canvas.drawTextOnPath(MY_TEXT, mArc, 0, 20, mPaintText);
-            invalidate();
-        }
+    private Paint mPaintText;
+    public OuterCircleTextView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+        setPath(20,20, 200, Path.Direction.CW, -180, 200);
+        setTextStyle(Paint.ANTI_ALIAS_FLAG, Paint.Style.FILL_AND_STROKE, Color.WHITE,20f);
     }
+    public OuterCircleTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context,attrs,defStyleAttr);
+        setPath(20,20, 200, Path.Direction.CW, -180, 200);
+        setTextStyle(Paint.ANTI_ALIAS_FLAG, Paint.Style.FILL_AND_STROKE, Color.WHITE,20f);
+    }
+
+    public OuterCircleTextView(Context context) {
+        super(context);
+        setPath(20,20, 200, Path.Direction.CW, -180, 200);
+        setTextStyle(Paint.ANTI_ALIAS_FLAG, Paint.Style.FILL_AND_STROKE, Color.WHITE,20f);
+
+    }
+
+
+    public void setText(String newText) {
+        MY_TEXT = newText;
+        invalidate();
+    }
+
+    public void setTextStyle (int flags, Paint.Style style, int color, float size) {
+        mPaintText = new Paint(flags);
+        mPaintText.setStyle(style);
+        mPaintText.setColor(color);
+        mPaintText.setTextSize(size);
+        invalidate();
+    }
+
+    public void setPath(int _x,
+                        int _y,
+                        float _radius,
+                        android.graphics.Path.Direction _direction,
+                        float _hOffset,
+                        float _vOffset) {
+        mArc = new Path();
+        mArc.addCircle(_x,_y, _radius, _direction);
+        hOffset = _hOffset;
+        vOffset = _vOffset;
+    }
+
+
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawTextOnPath(MY_TEXT, mArc, hOffset, vOffset, mPaintText);
+        invalidate();
+    }
+}
