7cfc3696e6466f008917bfb6860db41539f7301f
mrdale1958 <dale.macdonald@utdallas.edu>
2019-09-26 06:03:42 -0500

    Image hotspots add audio

--- app/src/main/java/com/botherconsulting/geoconnectable/Hotspot.java
+++ app/src/main/java/com/botherconsulting/geoconnectable/Hotspot.java
@@ -131,7 +131,7 @@ public class Hotspot {
         }
     }
 
-    private void reportZoomStats() {
+    protected void reportZoomStats() {
         long windowSum = 0;
         long windowSumSquared = 0;
         long maxEt = 0;
@@ -157,7 +157,7 @@ public class Hotspot {
         );
     }
 
-    private void updateZoomStats() {
+    protected void updateZoomStats() {
         long elapsedTime = System.nanoTime() - lastZoomMessageTime;
 
         lastZoomMessageTime = System.nanoTime();
@@ -171,7 +171,7 @@ public class Hotspot {
         }
     }
 
-    private void reportTiltStats() {
+    protected void reportTiltStats() {
         long windowSum = 0;
         long windowSumSquared = 0;
         long maxEt = 0;
@@ -191,7 +191,7 @@ public class Hotspot {
         );
     }
 
-    private void updateTiltStats() {
+    protected void updateTiltStats() {
         long elapsedTime = System.nanoTime() - lastTiltMessageTime;
         lastTiltMessageTime = System.nanoTime();
         eventTiltCount++;
@@ -206,137 +206,6 @@ public class Hotspot {
 
     public Boolean handleJSON(JSONObject message, GoogleMap mMap, boolean doLog)
         {
-            String gestureType;
-            try {
-                gestureType = message.getString("gesture");
-                //Log.i("incoming message",message.toString());
-            } catch (org.json.JSONException e) {
-                Log.i("GCT HS: no gesture msg",message.toString());
-                return false;
-            }
-            double deltaX = 0.0;
-
-            double deltaY = 0.0;
-            deltaZ = 0;
-            if (gestureType.equals("pan")) {
-                JSONObject vector = new JSONObject();
-                try {
-                    vector = message.getJSONObject("vector");
-                } catch (org.json.JSONException e) {
-                    Log.e("GCT HS: reading pan msg", "no vector " + message.toString());
-                }
-                try {
-                    //double screenWidthDegrees = Math.abs(curScreen.southwest.longitude - curScreen.northeast.longitude);
-                    //double screenHeightDegrees = Math.abs(curScreen.southwest.latitude - curScreen.northeast.latitude);
-                    double rawX = vector.getDouble("x");
-                    double percentChangeInX = 0;
-                    double percentChangeInY = 0;
-                    double rawY = vector.getDouble("y");
-
-                    if (doLog) {
-                        Log.i("GCT HS : pan update", " raw x: " + Double.toString(rawX) +
-                                " raw y: " + Double.toString(rawY));
-                    }
-                    if (Math.abs(rawX) < validTiltThreshold && Math.abs(rawY) < validTiltThreshold) {
-                        if (doLog) {
-                            Log.d("GCT HS: rejected pan", " raw x: " + Double.toString(rawX) +
-                                    " raw y: " + Double.toString(rawY) + " validTiltThreshold: " + Double.toString(validTiltThreshold));
-                        }
-                        return false;
-                    } else {
-
-                        //double zoomFudge = (minZoom + 7) +
-                        //        ((minZoom + 7) - (maxZoom-3 ))/(minZoom-maxZoom) *
-                        //                (mMap.getCameraPosition().zoom-minZoom);
-                        //Log.i("fudge", Double.toString(zoomFudge) + ":" +  Double.toString(mMap.getCameraPosition().zoom));
-                /*percentChangeInY = TiltScaleY * rawY *zoomFudge/maxZoom;
-                deltaY = screenHeightDegrees * percentChangeInY;
-
-                percentChangeInX = TiltScaleX * rawX *zoomFudge/maxZoom;
-                deltaX = screenWidthDegrees * percentChangeInX;*/
-                        deltaY = Math.min(Math.max(TiltScaleY * rawY, -panMax), panMax);
-
-                        deltaX = Math.min(Math.max(TiltScaleX * rawX, -panMax), panMax);
-                    }
-
-                    //if (zoomLayers[currentZoom]["pannable"])
-                    //Log.i("incoming pan",x + "," + y);
-                    if (doLog) {
-                        Log.i("GCT HS: pan hs update", " deltax: " + Double.toString(deltaX) +
-                                " deltay: " + Double.toString(deltaY)
-                        );
-                    }
-                    //mMap.animateCamera(CameraUpdateFactory.scrollBy((float) ( x), (float) ( y)));
-                    Double[] nextTilt = { deltaY, deltaX };
-                    if (nextTilt != currentTilt) {
-                        updateTiltStats();
-                        Double[]  position = currentTilt;
-                        currentTilt = nextTilt;
-                        newData = true;
-                    }
-                    //mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
-
-                } catch (org.json.JSONException e) {
-                    Log.e("GCT HS error: pan msg", "invalid vector " + vector.toString());
-                }
-            }
-            else
-            {
-                deltaZ = 0;
-                JSONObject vector = new JSONObject();
-                try {
-                    vector = message.getJSONObject("vector");
-                } catch (org.json.JSONException e) {
-                    Log.e("GCT HS error: zoom msg", "no vector " + message.toString());
-                }
-                try {
-                    deltaZ = vector.getInt("delta");
-                    int messageID = message.getInt("id");
-                    //if (messageID > lastMessageID + 1)
-                    //    Log.w("reading zoom data","got" + Integer.toString(messageID) + " after" + Integer.toString(lastMessageID));
-                    lastZoomMessageID = messageID;
-                } catch (org.json.JSONException e) {
-                    Log.e("GCT HS error: zoom msg", "invalid vector " + vector.toString());
-                }
-                currentSpinPosition += deltaZ;
-                currentSpinPosition = Math.max(minSpin,Math.min(currentSpinPosition,maxSpin));
-
-                double proposedZoom = idleZoom + (double)currentSpinPosition / (double)clicksPerZoomLevel;
-                if (doLog) {
-                    Log.i("GCT HS: zoom update", "delta:" + Integer.toString(deltaZ) +
-                            " new zoom: " +
-                            //String.format ("%.2d", proposedZoom) +
-                            proposedZoom +
-                            " cSP: " +
-                            Integer.toString(currentSpinPosition) +
-                            " min:" + Integer.toString(minSpin) +
-                            " max:" + Integer.toString(maxSpin)
-                    );
-                }
-
-                //restartIdleTimer();
-                updateZoomStats();
-
-                if (proposedZoom != currentZoom) {
-                    zoom = currentZoom;
-                    currentZoom = proposedZoom;
-                    newData = true;
-
-                }
-
-
-            }
- /*           if (newData)
-            {
-                displaySurface.setVisibility(View.VISIBLE);
-                String updateURL = "javascript://table.update(" + currentTilt + " , " + currentZoom + ")";
-                if (doLog) {
-                    Log.i("GCT HotSpot: notify webView" , updateURL);
-                }
-                displaySurface.loadUrl(updateURL);
-                return true;
-            }
-*/
-            return false;
+             return false;
         }
 }
