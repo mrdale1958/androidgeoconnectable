c140201ac7b8d038dc2b3b635014ea18cd6c79ab
mrdale1958 <dale.macdonald@utdallas.edu>
2019-10-09 10:08:09 -0500

    Make handleJSON() async

--- app/src/main/java/com/botherconsulting/geoconnectable/TablePanner.java
+++ app/src/main/java/com/botherconsulting/geoconnectable/TablePanner.java
@@ -91,91 +91,120 @@ public class TablePanner {
             reportStats();
         }
     }
+    private JSONObject message;
+    private boolean doLog;
+    private GoogleMap mMap;
+    private double screenWidthDegrees, screenHeightDegrees;
 
-    public void handleJSON(JSONObject message,
-                           GoogleMap mMap,
-                           boolean doLog,
-                           double screenWidthDegrees,
-                           double screenHeightDegrees){
-        //LatLngBounds curScreen = mMap.getProjection()
-        //        .getVisibleRegion().latLngBounds;
-        double deltaX = 0.0;
-        double deltaY = 0.0;
-        JSONObject vector = new JSONObject();
-        try {
-            vector = message.getJSONObject("vector");
-        } catch (org.json.JSONException e) {
-            Log.e("GCT pan: reading", "no vector " + message.toString());
-        }
-        try {
-            //double screenWidthDegrees = Math.abs(curScreen.southwest.longitude - curScreen.northeast.longitude);
-            //double screenHeightDegrees = Math.abs(curScreen.southwest.latitude - curScreen.northeast.latitude);
-            /*
-            *
-            * * Danger will robinson!  x and y swapped for SFSU installation
-            *
-            */
-            double rawX = vector.getDouble("y");
+    public void setMessage(JSONObject _message) {
+        this.message = _message;
+    }
 
-            double percentChangeInX = 0;
-            double percentChangeInY = 0;
-            double rawY = vector.getDouble("x");
+    public void setMap(GoogleMap _mMap) {
+        this.mMap = _mMap;
+    }
 
-            if (doLog) {
-                Log.i("GCT pan: incoming", " raw x: " + Double.toString(rawX) +
-                        " raw y: " + Double.toString(rawY) + " screenWidthDegrees: " + Double.toString(screenWidthDegrees) +
-                        " screenHeightDegrees: " + Double.toString(screenHeightDegrees) );
+    public void setLogging(boolean _doLog) {
+        this.doLog = _doLog;
+    }
+
+    public void setScreenBounds(double _screenWidthDegrees,
+                                double _screenHeightDegrees)
+    {
+        this.screenWidthDegrees = _screenWidthDegrees;
+        this.screenHeightDegrees = _screenHeightDegrees;
+    }
+
+    public final Runnable  handleJSON  = new Runnable() {
+
+
+        public void run() {
+//    public void handleJSON(JSONObject message,
+            //                       GoogleMap mMap,
+            //                       boolean doLog,
+            //                       double screenWidthDegrees,
+            //                       double screenHeightDegrees){
+            //LatLngBounds curScreen = mMap.getProjection()
+            //        .getVisibleRegion().latLngBounds;
+            double deltaX = 0.0;
+            double deltaY = 0.0;
+            JSONObject vector = new JSONObject();
+            try {
+                vector = message.getJSONObject("vector");
+            } catch (org.json.JSONException e) {
+                Log.e("GCT pan: reading", "no vector " + message.toString());
             }
-            if (Math.abs(rawX) < validTiltThreshold && Math.abs(rawY) < validTiltThreshold) {
-                if (doLog) {
-                    Log.d("GCT pan: rejected", " raw x: " + Double.toString(rawX) +
-                            " raw y: " + Double.toString(rawY) + " validTiltThreshold: " + Double.toString(validTiltThreshold));
-                }
-                return;
-            } else {
+            try {
+                //double screenWidthDegrees = Math.abs(curScreen.southwest.longitude - curScreen.northeast.longitude);
+                //double screenHeightDegrees = Math.abs(curScreen.southwest.latitude - curScreen.northeast.latitude);
+                /*
+                 *
+                 * * Danger will robinson!  x and y swapped for SFSU installation
+                 *
+                 */
+                double rawX = vector.getDouble("y");
 
-                //double zoomFudge = (minZoom + 7) +
-                //        ((minZoom + 7) - (maxZoom-3 ))/(minZoom-maxZoom) *
-                //                (mMap.getCameraPosition().zoom-minZoom);
-                //Log.i("fudge", Double.toString(zoomFudge) + ":" +  Double.toString(mMap.getCameraPosition().zoom));
+                double percentChangeInX = 0;
+                double percentChangeInY = 0;
+                double rawY = vector.getDouble("x");
+
+                if (doLog) {
+                    Log.i("GCT pan: incoming", " raw x: " + Double.toString(rawX) +
+                            " raw y: " + Double.toString(rawY) + " screenWidthDegrees: " + Double.toString(screenWidthDegrees) +
+                            " screenHeightDegrees: " + Double.toString(screenHeightDegrees));
+                }
+                if (Math.abs(rawX) < validTiltThreshold && Math.abs(rawY) < validTiltThreshold) {
+                    if (doLog) {
+                        Log.d("GCT pan: rejected", " raw x: " + Double.toString(rawX) +
+                                " raw y: " + Double.toString(rawY) + " validTiltThreshold: " + Double.toString(validTiltThreshold));
+                    }
+                    return;
+                } else {
+
+                    //double zoomFudge = (minZoom + 7) +
+                    //        ((minZoom + 7) - (maxZoom-3 ))/(minZoom-maxZoom) *
+                    //                (mMap.getCameraPosition().zoom-minZoom);
+                    //Log.i("fudge", Double.toString(zoomFudge) + ":" +  Double.toString(mMap.getCameraPosition().zoom));
                 /*percentChangeInY = TiltScaleY * rawY *zoomFudge/maxZoom;
                 deltaY = screenHeightDegrees * percentChangeInY;
 
                 percentChangeInX = TiltScaleX * rawX *zoomFudge/maxZoom;
                 deltaX = screenWidthDegrees * percentChangeInX;*/
-                percentChangeInY = Math.min(Math.max(TiltScaleY * rawY, -panMax), panMax) ;
-                deltaY = screenHeightDegrees * percentChangeInY;
+                    percentChangeInY = Math.min(Math.max(TiltScaleY * rawY, -panMax), panMax);
+                    deltaY = screenHeightDegrees * percentChangeInY;
 
-                percentChangeInX = Math.min(Math.max(TiltScaleX * rawX, -panMax), panMax);
-                deltaX = screenWidthDegrees * percentChangeInX;
+                    percentChangeInX = Math.min(Math.max(TiltScaleX * rawX, -panMax), panMax);
+                    deltaX = screenWidthDegrees * percentChangeInX;
+                }
+
+                //if (zoomLayers[currentZoom]["pannable"])
+                //Log.i("incoming pan",x + "," + y);
+                if (doLog) {
+                    Log.i("GCT: pan update", "%x: " + Double.toString(percentChangeInX) +
+                            " %y: " + Double.toString(percentChangeInY) +
+                            " deltax: " + Double.toString(deltaX) +
+                            " deltay: " + Double.toString(deltaY) +
+                            " current Lat:" +
+                            Double.toString(mMap.getCameraPosition().target.latitude) +
+                            " current Lon:" +
+                            Double.toString(mMap.getCameraPosition().target.longitude)
+
+                    );
+                }
+                //mMap.animateCamera(CameraUpdateFactory.scrollBy((float) ( x), (float) ( y)));
+                LatLng currentCameraPosition = mMap.getCameraPosition().target;
+                LatLng nextPosition = new LatLng(currentCameraPosition.latitude + deltaY, currentCameraPosition.longitude + deltaX);
+                if (nextPosition != currentCameraPosition) {
+                    updateStats(doLog);
+                    position = currentPosition;
+                    currentPosition = nextPosition;
+                    newData = true;
+                }
+                //mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+
+            } catch (org.json.JSONException e) {
+                Log.e("GCT pan: reading", "invalid vector " + vector.toString());
             }
-
-            //if (zoomLayers[currentZoom]["pannable"])
-            //Log.i("incoming pan",x + "," + y);
-            if (doLog) {
-                Log.i("GCT: pan update", "%x: " + Double.toString(percentChangeInX) +
-                        " %y: " + Double.toString(percentChangeInY) +
-                        " deltax: " + Double.toString(deltaX) +
-                        " deltay: " + Double.toString(deltaY) +
-                        " current Lat:" +
-                        Double.toString(mMap.getCameraPosition().target.latitude) +
-                        " current Lon:" +
-                        Double.toString(mMap.getCameraPosition().target.longitude)
-
-                );
-            }
-            //mMap.animateCamera(CameraUpdateFactory.scrollBy((float) ( x), (float) ( y)));
-            LatLng currentCameraPosition = mMap.getCameraPosition().target;
-            LatLng  nextPosition = new LatLng(currentCameraPosition.latitude+deltaY, currentCameraPosition.longitude + deltaX);
-            if (nextPosition != currentCameraPosition) {
-                updateStats(doLog);
-                position = currentPosition;
-                currentPosition = nextPosition;
-                newData = true;
-            }
-            //mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
-
-        } catch (org.json.JSONException e) {
-            Log.e("GCT pan: reading", "invalid vector " + vector.toString());
-    }
-}}
+        }
+    };
+}
